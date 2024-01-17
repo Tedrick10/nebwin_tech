ff34529d0eac29557be31d854b032367126f3d66
Thet Htun Kyaw <thettunkyaw.developer@gmail.com>
2024-01-17 22:17:36 +0630

    Changed the title in main.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // Returning Widgets
     return MaterialApp(
-      title: "Nebwin Tech",
+      title: "Nebwin Tech Website",
       debugShowCheckedModeBanner: false,
       initialRoute: Routes.initialRoute,
       routes: Routes.routes,
